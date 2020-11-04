c2176b70192047339619b20bf6695ddb78ba2ad1
Deepak Gopalan <gopalan.d@northeastern.edu>
2020-11-04 05:18:55 -0500

    fixing long running process issue in applicationstart.sh script

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Start the Spring boot app running
-java -jar ~/target/project-0.0.1-SNAPSHOT.jar >> app.log 2>&1 &
+java -jar ~/target/project-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
