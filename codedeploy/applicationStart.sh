24d5c29e42262dff06e301122488b9003491d917
Deepak Gopalan <gopalan.d@northeastern.edu>
2020-11-12 10:58:59 -0500

    forwarding console logs to new file

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -4,4 +4,4 @@
 #sudo touch /home/ubuntu/logs/csye6225.log
 #sudo chmod 666 /home/ubuntu/logs/csye6225.log
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/config.json -s
-sudo java -jar /home/ubuntu/target/project-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+sudo java -jar /home/ubuntu/target/project-0.0.1-SNAPSHOT.jar > logs.log 2> logs.log < logs.log &
