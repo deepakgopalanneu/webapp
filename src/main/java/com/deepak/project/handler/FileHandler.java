8eca756a77abda964b7111641ed7f2979312017a
Deepak Gopalan <gopalan.d@northeastern.edu>
2020-11-12 01:51:35 -0500

    all logging and metrics added

--- src/main/java/com/deepak/project/handler/FileHandler.java
+++ src/main/java/com/deepak/project/handler/FileHandler.java
@@ -5,6 +5,9 @@ import com.deepak.project.Exception.QuestionException;
 import com.deepak.project.model.File;
 import com.deepak.project.model.UserPrincipal;
 import com.deepak.project.service.FileService;
+import com.timgroup.statsd.StatsDClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,41 +21,104 @@ import java.security.Principal;
 @RestController
 public class FileHandler {
 
-    @Autowired
-    private FileService fileService;
 
+    private FileService fileService;
+    private final static Logger logger = LoggerFactory.getLogger(FileHandler.class);
+    StatsDClient statsd;
+
+    @Autowired
+    public FileHandler(FileService fileService, StatsDClient statsd) {
+        this.fileService = fileService;
+        this.statsd = statsd;
+    }
+
+    /**
+     * This method saves file to question, uploads to S3 and saves File info in DB
+     * @param uploadedFile
+     * @param questionId
+     * @param principal
+     * @return
+     * @throws FileException
+     * @throws QuestionException
+     */
     @PostMapping("/v1/question/{question_id}/file")
     public ResponseEntity<File> postImageToQuestion(@RequestPart @NotNull MultipartFile uploadedFile,
                                                     @PathVariable("question_id") @NotNull String questionId, Principal principal) throws FileException, QuestionException {
+        logger.info("Logging from POST /v1/question/{id}/file controller method");
+        long startTime = System.currentTimeMillis();
+        statsd.increment("Traffic - POST /v1/question/{id}/file");
         UserPrincipal userPrincipal = (UserPrincipal) ((Authentication) principal).getPrincipal();
         String userId = userPrincipal.getId();
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(fileService.saveFileToQuestion(uploadedFile, userId, questionId));
+        File f = fileService.saveFileToQuestion(uploadedFile, userId, questionId);
+        statsd.recordExecutionTime("ResponseTime - POST /v1/question/{id}/file",System.currentTimeMillis() - startTime);
+        return ResponseEntity.status(HttpStatus.CREATED).body(f);
     }
 
+    /**
+     * This method deletes the file with the given fileId from the question with given questionId
+     * @param questionId
+     * @param fileId
+     * @param principal
+     * @return
+     * @throws FileException
+     * @throws QuestionException
+     */
     @DeleteMapping("/v1/question/{question_id}/file/{file_id}")
     public ResponseEntity deleteImageFromQuestion(@PathVariable("question_id") @NotNull String questionId,
                                                   @PathVariable("file_id") @NotNull String fileId, Principal principal) throws FileException, QuestionException {
+        logger.info("Logging from DELETE /v1/question/{id}/file/{id} controller method");
+        long startTime = System.currentTimeMillis();
+        statsd.increment("Traffic - DELETE /v1/question/{id}/file/{id}");
         UserPrincipal userPrincipal = (UserPrincipal) ((Authentication) principal).getPrincipal();
         String userId = userPrincipal.getId();
         fileService.deleteFromQuestion(userId, questionId, fileId);
+        statsd.recordExecutionTime("ResponseTime - DELETE /v1/question/{id}/file/{id}",System.currentTimeMillis() - startTime);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
+    /**
+     *  This method saves file to Answer, uploads to S3 and saves File info in DB
+     * @param uploadedFile
+     * @param answerId
+     * @param questionId
+     * @param principal
+     * @return
+     * @throws FileException
+     * @throws QuestionException
+     */
     @PostMapping("/v1/question/{question_id}/answer/{answer_id}/file")
     public ResponseEntity<File> postImageToAnswer(@RequestPart @NotNull MultipartFile uploadedFile, @PathVariable("answer_id") @NotNull String answerId,
                                                   @PathVariable("question_id") @NotNull String questionId, Principal principal) throws FileException, QuestionException {
+        logger.info("Logging from POST /v1/question/{id}/answer/{id}/file controller method");
+        long startTime = System.currentTimeMillis();
+        statsd.increment("Traffic - POST /v1/question/{id}/answer/{id}/file");
         UserPrincipal userPrincipal = (UserPrincipal) ((Authentication) principal).getPrincipal();
         String userId = userPrincipal.getId();
-        return ResponseEntity.status(HttpStatus.CREATED).body(fileService.saveFileToAnswer(uploadedFile, userId, questionId, answerId));
+        File f = fileService.saveFileToAnswer(uploadedFile, userId, questionId, answerId);
+        statsd.recordExecutionTime("ResponseTime - POST /v1/question/{id}/answer/{id}/file",System.currentTimeMillis() - startTime);
+        return ResponseEntity.status(HttpStatus.CREATED).body(f);
     }
 
+    /**
+     * This method deletes the file with the given fileId from the Answer with given AnswerId
+     * @param questionId
+     * @param answerId
+     * @param fileId
+     * @param principal
+     * @return
+     * @throws FileException
+     * @throws QuestionException
+     */
     @DeleteMapping("/v1/question/{question_id}/answer/{answer_id}/file/{file_id}")
     public ResponseEntity deleteImageFromAnswer(@PathVariable("question_id") @NotNull String questionId, @PathVariable("answer_id") @NotNull String answerId,
                                                 @PathVariable("file_id") @NotNull String fileId, Principal principal) throws FileException, QuestionException {
+        logger.info("Logging from DELETE /v1/question/{id}/answer/{id}/file/{id} controller method");
+        long startTime = System.currentTimeMillis();
+        statsd.increment("Traffic - DELETE /v1/question/{id}/answer/{id}/file/{id}");
         UserPrincipal userPrincipal = (UserPrincipal) ((Authentication) principal).getPrincipal();
         String userId = userPrincipal.getId();
         fileService.deleteFromAnswer(userId, questionId, answerId, fileId);
+        statsd.recordExecutionTime("ResponseTime - DELETE /v1/question/{id}/answer/{id}/file/{id}",System.currentTimeMillis() - startTime);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
--- src/main/java/com/deepak/project/service/QuestionService.java
+++ src/main/java/com/deepak/project/service/QuestionService.java
@@ -10,6 +10,9 @@ import com.deepak.project.repository.AnswerRepository;
 import com.deepak.project.repository.CategoryRepository;
 import com.deepak.project.repository.QuestionRepository;
 import com.deepak.project.util.CustomStrings;
+import com.timgroup.statsd.StatsDClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -27,14 +30,21 @@ import java.util.regex.Pattern;
 @Service
 public class QuestionService {
 
-    @Autowired
     QuestionRepository questionRepo;
-    @Autowired
     CategoryRepository categoryRepo;
-    @Autowired
     AnswerRepository answerRepo;
-    @Autowired
     FileService fileService;
+    private final static Logger logger = LoggerFactory.getLogger(QuestionService.class);
+    StatsDClient statsd;
+
+    @Autowired
+    public QuestionService(QuestionRepository questionRepo, CategoryRepository categoryRepo, AnswerRepository answerRepo, FileService fileService, StatsDClient statsd) {
+        this.questionRepo = questionRepo;
+        this.categoryRepo = categoryRepo;
+        this.answerRepo = answerRepo;
+        this.fileService = fileService;
+        this.statsd = statsd;
+    }
 
     public static <T> Predicate<T> distinctByKey(
             Function<? super T, ?> keyExtractor) {
@@ -44,6 +54,7 @@ public class QuestionService {
     }
 
     public Question createQuestion(Question question, String userId) throws QuestionException {
+        logger.info("Logging from CREATE Question service method");
         question.setCreated_timestamp(LocalDateTime.now().toString());
         question.setUpdated_timestamp(LocalDateTime.now().toString());
         question.setUserId(userId);
@@ -55,6 +66,7 @@ public class QuestionService {
             givenCategories = new ArrayList<>();
         question.setCategories(null);
         try {
+
             givenCategories.stream().filter(distinctByKey(c -> c.getCategory())).forEach((category) -> {
                 String categoryName = category.getCategory().toLowerCase().trim();
                 if (null == categoryName || categoryName.trim().isEmpty())
@@ -76,13 +88,17 @@ public class QuestionService {
             question.setCategories(categoryList);
             question.setAnswers(new ArrayList<>());
             question.setAttachments(new ArrayList<>());
-            return questionRepo.save(question);
+            long startTime = System.currentTimeMillis();
+            Question q = questionRepo.save(question);
+            statsd.recordExecutionTime("DB ResponseTime - SAVE Question", System.currentTimeMillis() - startTime);
+            return q;
         } catch (Exception ex) {
             throw new QuestionException(ex.getMessage());
         }
     }
 
     public Answer answerQuestion(String question_id, String userId, String answer_text) throws QuestionException {
+        logger.info("Logging from CREATE ANSWER service method");
         Answer answer = new Answer();
         answer.setAnswer_text(answer_text);
         answer.setUserId(userId);
@@ -91,10 +107,13 @@ public class QuestionService {
         answer.setUpdated_timestamp(LocalDateTime.now().toString());
         answer.setAttachments(new ArrayList<>());
         try {
+            long startTime = System.currentTimeMillis();
             Optional<Question> q = questionRepo.findById(question_id);
-            if (q.isPresent())
-                return answerRepo.save(answer);
-            else
+            if (q.isPresent()) {
+                Answer ans =  answerRepo.save(answer);
+                statsd.recordExecutionTime("DB ResponseTime - SAVE ANSWER", System.currentTimeMillis() - startTime);
+                return ans;
+            }else
                 throw new QuestionException(CustomStrings.not_found);
         } catch (Exception e) {
             throw new QuestionException(e.getMessage());
@@ -102,7 +121,7 @@ public class QuestionService {
     }
 
     public void editQuestion(Question question, String question_id, String userId) throws QuestionException {
-
+        logger.info("Logging from EDIT Question service method");
         try {
             Optional<Question> foundQuestion = questionRepo.findById(question_id);
             if (foundQuestion.isPresent()) {
@@ -138,7 +157,9 @@ public class QuestionService {
                     q.setCategories(categoryList);
                     q.setQuestion_text(question.getQuestion_text());
                     q.setUpdated_timestamp(LocalDateTime.now().toString());
+                    long startTime = System.currentTimeMillis();
                     questionRepo.save(q);
+                    statsd.recordExecutionTime("DB ResponseTime - EDIT ANSWER", System.currentTimeMillis() - startTime);
                 }
             } else {
                 throw new QuestionException(CustomStrings.not_found);
@@ -149,6 +170,8 @@ public class QuestionService {
     }
 
     private void deleteImagesFromS3(List<File> attachments) {
+        logger.info("Logging from DELETE IMAGES FROM S3 when question is deleted - service method");
+        long startTime = System.currentTimeMillis();
         attachments.stream().forEach(f -> {
             try {
                 fileService.deleteImageBys3ObjectName(f.getS3ObjectName());
@@ -156,9 +179,12 @@ public class QuestionService {
                 throw new RuntimeException("Unable to Delete Images attached to this Question");
             }
         });
+        statsd.recordExecutionTime("S3 ResponseTime - DELETE File FROM S3", System.currentTimeMillis() - startTime);
+
     }
 
     public void deleteQuestion(String question_id, String userId) throws QuestionException {
+        logger.info("Logging from DELETE Question service method");
         try {
             Optional<Question> foundQuestion = questionRepo.findById(question_id);
             if (foundQuestion.isPresent()) {
@@ -170,7 +196,10 @@ public class QuestionService {
                     if (q.getAttachments().size() > 0) {
                         deleteImagesFromS3(q.getAttachments());
                     }
+                    long startTime = System.currentTimeMillis();
                     questionRepo.deleteById(question_id);
+                    statsd.recordExecutionTime("DB ResponseTime - DELETE Question", System.currentTimeMillis() - startTime);
+
                 } else {
                     throw new QuestionException("You cannot delete a Question which has answers");
                 }
@@ -183,6 +212,7 @@ public class QuestionService {
     }
 
     public void updateAnswer(String question_id, String answer_id, String answer_text, String userId) throws QuestionException {
+        logger.info("Logging from UPDATE Answer service method");
         try {
             Optional<Question> questOptional = questionRepo.findById(question_id);
             Answer ans = null;
@@ -196,7 +226,10 @@ public class QuestionService {
                     } else {
                         ans.setAnswer_text(answer_text);
                         ans.setUpdated_timestamp(LocalDateTime.now().toString());
+                        long startTime = System.currentTimeMillis();
                         answerRepo.save(ans);
+                        statsd.recordExecutionTime("DB ResponseTime - UPDATE Answer", System.currentTimeMillis() - startTime);
+
                     }
                 } else {
                     throw new QuestionException(CustomStrings.answer_notfound);
@@ -210,6 +243,7 @@ public class QuestionService {
     }
 
     public void deleteAnswer(String answer_id, String question_id, String userId) throws QuestionException {
+        logger.info("Logging from DELETE Answer service method");
         try {
             Optional<Question> questOptional = questionRepo.findById(question_id);
             Answer ans;
@@ -223,7 +257,10 @@ public class QuestionService {
                     } else {
                         if (ans.getAttachments().size() > 0)
                             deleteImagesFromS3(ans.getAttachments());
+                        long startTime = System.currentTimeMillis();
                         answerRepo.deleteById(ans.getAnswer_id());
+                        statsd.recordExecutionTime("DB ResponseTime - DELETE Answer", System.currentTimeMillis() - startTime);
+
                     }
                 } else {
                     throw new QuestionException(CustomStrings.answer_notfound);
@@ -237,8 +274,12 @@ public class QuestionService {
     }
 
     public Question getQuestion(String question_id) throws QuestionException {
+        logger.info("Logging from GET Question service method");
         try {
+            long startTime = System.currentTimeMillis();
             Optional<Question> foundQuestion = questionRepo.findById(question_id);
+            statsd.recordExecutionTime("DB ResponseTime - GET QUESTION", System.currentTimeMillis() - startTime);
+
             if (foundQuestion.isPresent()) {
                 return foundQuestion.get();
             } else {
@@ -250,8 +291,11 @@ public class QuestionService {
     }
 
     public List<Question> getAllQuestions() throws QuestionException {
+        logger.info("Logging from GET ALL Questions service method");
         try {
+            long startTime = System.currentTimeMillis();
             List<Question> foundQuestions = (List<Question>) questionRepo.findAll();
+            statsd.recordExecutionTime("DB ResponseTime - GET ALL Questions", System.currentTimeMillis() - startTime);
             if (foundQuestions.isEmpty()) {
                 throw new QuestionException(CustomStrings.not_found);
             } else {
@@ -263,8 +307,11 @@ public class QuestionService {
     }
 
     public Answer getAnswer(String answer_id) throws QuestionException {
+        logger.info("Logging from GET Answer service method");
         try {
+            long startTime = System.currentTimeMillis();
             Optional<Answer> foundAnswer = answerRepo.findById(answer_id);
+            statsd.recordExecutionTime("DB ResponseTime - GET Answer", System.currentTimeMillis() - startTime);
             if (foundAnswer.isPresent()) {
                 Answer ans = foundAnswer.get();
                 return ans;
