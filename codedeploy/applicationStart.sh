7881568a654de1223ff452aacd5d198b4712f764
Deepak Gopalan <gopalan.d@northeastern.edu>
2020-11-12 01:59:43 -0500

    edited applicatioStart script

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Start the Spring boot app running
-sudo mkdir logs
-sudo touch logs/csye6225.log
-java -jar ~/target/project-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+sudo mkdir /home/ubuntu/logs
+sudo touch /home/ubuntu/logs/csye6225.log
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/config.json -s
+java -jar ~/target/project-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
