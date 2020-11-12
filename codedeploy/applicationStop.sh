a66c5e71bd6d06b09c62036386e4e9ed2908a568
Deepak Gopalan <gopalan.d@northeastern.edu>
2020-11-12 11:38:06 -0500

    added afterinstall to lifcycle hook

--- codedeploy/applicationStop.sh
+++ codedeploy/applicationStop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Stop the Spring boot app running
 sudo pkill -f 'java -jar'
-sudo rm -rf logs/
 sudo rm -rf target/
 sudo rm -rf codedeploy/
 sudo rm -f appspec.yml
+sudo rm -rf logs/
